14a5a0a159dbc74fd352d950f986ead168d87617
Juno Julius <yunoyulius@gmail.com>
2019-01-16 18:52:00 +0100

    updated padding temp fix for nav

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -10,10 +10,10 @@ render(){
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
       <h2 className="my-0 mr-md-auto font-weight-normal">Nika <small className="text-muted">van Berkel</small></h2>
       <nav className="my-2 my-md-0 mr-md-3">
-      <a className="p-4 h5 text-dark" href="/bio">Bio</a>
-        <a className="p-4 h5 text-dark" href="/projects">Projects</a>
-        <a className="p-4 h5 text-dark" href="/news">News</a>
-        <a className="p-4 h5 text-dark" href="/contact">Contact</a>
+      <a className="p-2 h5 text-dark" href="/bio">Bio</a>
+        <a className="p-2 h5 text-dark" href="/projects">Projects</a>
+        <a className="p-2 h5 text-dark" href="/news">News</a>
+        <a className="p-2 h5 text-dark" href="/contact">Contact</a>
       </nav>
     </div>
       </header>
