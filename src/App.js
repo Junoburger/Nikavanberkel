5eccf8b896284e9f51c906fc83ade41176492339
Juno Julius <yunoyulius@gmail.com>
2019-01-16 15:58:21 +0100

    made it better

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Header} from './components/UI/Header';
-import {Example} from './components/Card/Card'
+import {Example} from './components/Card/Card';
+
 
 class App extends Component {
   render() {
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,7 +6,7 @@ export const Example = (props) => {
   return (
     <div>
       <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <CardImg top width="100%" alt="Card image cap" />
         <CardBody>
           <CardTitle>Card title</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
