ec94b8531a3169abe11d438afa1dfdff3ccc5dd9
Juno Julius <yunoyulius@gmail.com>
2019-01-21 18:52:55 +0100

    added flex rows plus hover effect

--- src/components/UI/ProjectInfo/ProjectGallery.js
+++ src/components/UI/ProjectInfo/ProjectGallery.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import {projects} from './projectObjects'
 import './ProjectInfo.css'
 
