07acb6845fc961edb8076fb3d63699d67acb3ae6
Juno Burger <juno.burger@appythings.nl>
2019-01-21 11:50:22 +0100

    added hover effect with project title

--- src/components/UI/ProjectInfo/ProjectGallery.js
+++ src/components/UI/ProjectInfo/ProjectGallery.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button } from 'reactstrap';
+    CardTitle, CardSubtitle } from 'reactstrap';
 import {projects} from './projectObjects'
-
+import './ProjectInfo.css'
 
 
 let divStyle = {
     padding:'20px'
   };
     
-const showInfo = () =>{
-console.log("determine action")
-}
-
 
    export const gallery = projects.map((project) => {
         return (
   
             <div>
-            <div style={divStyle}>
+            <div style={divStyle} >
               <Card>
+              <div className="img__wrap">
                 <CardImg  top width="100%" src ={project.src}  alt={project.altText} />
                 <CardBody>
-                  <CardTitle>{project.title}</CardTitle>
-                  <CardSubtitle>{project.subTitle}</CardSubtitle>
-                  <CardText>{project.description}</CardText>
-                  <Button onClick={showInfo}>Button</Button>
+                <div className="img__description_layer">
+                  <CardTitle className="img__description">{project.title}</CardTitle>
+                </div>
+                  <CardSubtitle >{project.subTitle}</CardSubtitle>
+                  <CardText >{project.description}</CardText>
                 </CardBody>
+                </div>
               </Card>
-            </div>
-         
+             </div>
             </div>
           );
       
--- src/components/UI/ProjectInfo/projectObjects.js
+++ src/components/UI/ProjectInfo/projectObjects.js
@@ -5,21 +5,21 @@ import { image1, image2, image3 } from './projectImages';
     {
     src: image1,
     altText: 'image1',
-    title: 'Image 1',
+    title: 'ALB_view 1',
     subTitle: 'short intro 1',
     description: 'a short description 1'
     },
     {
     src: image2,
     altText: 'image2',
-    title: 'Image 2',
+    title: 'ALB_view 2',
     subTitle: 'short intro 2',
     description: 'a short description 2'
     }, 
     {
     src: image3,
     altText: 'image3',
-    title: 'Image 3',
+    title: 'ALB_view 3',
     subTitle: 'short intro 3',
     description: 'a short description 3'
     }
