631180a75e6ab133568f897e237db19033fdcfe6
Juno Burger <juno.burger@appythings.nl>
2019-01-28 15:47:16 +0100

    carousel main view projects

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,25 +4,85 @@ import Tree from 'react-d3-tree';
 import {MainData} from './MainData.js'
 import {containerStyles} from './ContainerStyles'
 import {svgSquare} from './SVGSquare'
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+import {items} from '../UI/ProjectInfo/projectObjects'
+
  
   export default class Main extends React.PureComponent{
+    constructor(props) {
+      super(props);
+      this.state = { activeIndex: 0 };
+      this.next = this.next.bind(this);
+      this.previous = this.previous.bind(this);
+      this.goToIndex = this.goToIndex.bind(this);
+      this.onExiting = this.onExiting.bind(this);
+      this.onExited = this.onExited.bind(this);
+    }
+    onExiting() {
+      this.animating = true;
+    }
+  
+    onExited() {
+      this.animating = false;
+    }
+  
+    next() {
+      if (this.animating) return;
+      const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+      this.setState({ activeIndex: nextIndex });
+    }
+  
+    previous() {
+      if (this.animating) return;
+      const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+      this.setState({ activeIndex: nextIndex });
+    }
+  
+    goToIndex(newIndex) {
+      if (this.animating) return;
+      this.setState({ activeIndex: newIndex });
+    }
 
-    state = {}
-    componentDidMount() {
-        const dimensions = this.treeContainer.getBoundingClientRect();
-        this.setState({
-          translate: {
-            x: dimensions.width / 2,
-            y: dimensions.height / 2
-          }
-        });
-      }
+    // state = {}
+    // componentDidMount() {
+    //     const dimensions = this.treeContainer.getBoundingClientRect();
+    //     this.setState({
+    //       translate: {
+    //         x: dimensions.width / 2,
+    //         y: dimensions.height / 2
+    //       }
+    //     });
+    //   }
 
       render() {
+        const { activeIndex } = this.state;
+        const slides = items.map((item) => {
+          return (
+            <CarouselItem
+              onExiting={this.onExiting}
+              onExited={this.onExited}
+              key={item.src}
+            >
+              <img src={item.src} alt={item.altText} />
+
+              <CarouselCaption
+             captionText={item.caption}
+             captionHeader={item.title} />
+            </CarouselItem>
+          );
+        });
+
       return(
         <div>
-        <div style={{display:'flex', justifyContent:'center'}}>
-        <div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
+           <div style={{display:'flex', justifyContent:'center'}}>
+
+        {/* <div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
         <Tree 
           data={MainData} 
           translate={this.state.translate} 
@@ -31,8 +91,29 @@ import {svgSquare} from './SVGSquare'
           nodeSvgShape={svgSquare}
           textLayout={{textAnchor: "start", x: 25, y: 0, transform: undefined }}
         />
-      </div>
-      </div>
+      </div> */}
+      
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators 
+        items={items}
+        activeIndex={activeIndex} 
+        onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl
+         direction="prev"
+         directionText="Previous"
+         onClickHandler={this.previous} />
+
+        <CarouselControl 
+        direction="next"
+         directionText="Next"
+         onClickHandler={this.next} />
+        </Carousel>
+          </div>
         </div>
     )
       }
