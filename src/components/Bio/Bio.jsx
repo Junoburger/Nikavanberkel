86685d0ca80fdbe66fe3764efdbb386ca9ec6503
JunoJulius <juno.burger@officebooking.net>
2019-09-20 10:49:55 +0200

    tiny tweaks will create a masterpiece

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -8,9 +8,7 @@ export const Bio = (props) => {
          <div className="border">
              <h1>About</h1>
              <p>
-                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Enim, cumque autem dolor cum nam suscipit officiis, laudantium maiores inventore ipsam 
-                 consequuntur quidem, velit quisquam. Nemo, delectus blanditiis! Labore, molestiae in.
+             Lorem ipsum dolor amet craft beer vinyl readymade meh, gentrify austin chartreuse messenger bag banjo XOXO locavore hella cliche dreamcatcher. Shabby chic locavore ugh authentic cliche art party waistcoat asymmetrical retro gentrify tacos banjo fixie. Af woke irony cold-pressed hoodie. Banh mi fam succulents, vape DIY cray occupy bespoke microdosing adaptogen cold-pressed YOLO selvage af. Godard poke PBR&B crucifix, biodiesel tattooed hot chicken yr. Lumbersexual try-hard put a bird on it four dollar toast chambray iPhone. Woke cloud bread next level whatever single-origin coffee, pop-up swag kombucha kitsch.
              </p>
         </div>
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Main.css'
  import examplePic from '../../assets/images/ALB_view 1.jpg'
- import examplePic2 from '../../assets/images/ALB_plans_171219_b&w.jpg'
+ import examplePic1 from '../../assets/images/ALB_view 2.jpg'
+ import examplePic2 from '../../assets/images/ALB_view 3.jpg'
+ import examplePic3 from '../../assets/images/ALB_plans_171219_b&w.jpg'
 
 
 
@@ -16,13 +18,11 @@ import './Main.css'
 <div class="Grid">
   <div class="Grid-item"><img  src={examplePic}/></div>
   <div class="Grid-item"><img  src={examplePic2}/></div>
-  <div class="Grid-item">example</div>
-  <div class="Grid-item">example</div>
-  <div class="Grid-item">example</div>
-  <div class="Grid-item">example</div>
-  <div class="Grid-item">example</div>
-  <div class="Grid-item">example</div>
-  <div class="Grid-item">example</div>
+  <div class="Grid-item"><img  src={examplePic3}/></div>
+  <div class="Grid-item"><img  src={examplePic1}/></div>
+  <div class="Grid-item"><img  src={examplePic}/></div>
+  <div class="Grid-item"><img  src={examplePic2}/></div>
+
 </div>
         </div>
     )
