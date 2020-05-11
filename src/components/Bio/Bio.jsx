aa95a48eab77e7d1bcef75234fc70424d39763c6
Juno <juno.burger@pm.me>
2020-05-11 12:44:13 +0200

    meta

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { About } from "../../styled_components/About";
 
-export const Bio = props => {
+export const Bio = (props) => {
   return (
     <>
       <About>
         <p>+31 6 38 45 63 49</p>
+        <br />
+        <p>nika.van.berkel@gmail.com</p>
       </About>
     </>
   );
