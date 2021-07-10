2b72c533e413db5b9743cd7d50eb32368530056e
Junoburger <juno.burger@pm.me>
2021-07-10 23:44:49 +0200

    Dynamically set the height of project images

--- src/App.tsx
+++ src/App.tsx
@@ -17,52 +17,61 @@ const Bookholder = React.lazy(() => import("./components/projects/Bookholder/Boo
 const Albania = React.lazy(() => import("./components/projects/Albania/Albania"));
 const SpaceForOne = React.lazy(() => import("./components/projects/Space for one/SpaceForOne"));
 const TwoDimensionalVase = React.lazy(
-  () => import("./components/projects/Two dimensional vase/TwoDimensionalVase")
+	() => import("./components/projects/Two dimensional vase/TwoDimensionalVase")
 );
 const MetropolitanArcadia = React.lazy(
-  () => import("./components/projects/Metropolitan Arcadia/MetropolitanArcadia")
+	() => import("./components/projects/Metropolitan Arcadia/MetropolitanArcadia")
 );
 // END LAZY IMPORTS
 
 const App: () => JSX.Element = () => {
-  const style = {
-    // width: " 100%",
-    // margin: "0 auto",
-    // display: "inline-block",
-    // maxWidth: "100%",
-    // height: "auto",
-    // width: "auto",
-  };
-
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <>
-        <Header></Header>
-        <div style={style}>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/about" component={Bio} />
-          <Suspense fallback={<LoadingState />}>
-            <Route exact path="/albania" component={Albania} />
-          </Suspense>
-          <Suspense fallback={<LoadingState />}>
-            <Route exact path="/bookholder" component={Bookholder} />
-          </Suspense>
-          <Suspense fallback={<LoadingState />}>
-            <Route exact path="/posy" component={Posy} />
-          </Suspense>
-          <Suspense fallback={<LoadingState />}>
-            <Route exact path="/spaceforone" component={SpaceForOne} />
-          </Suspense>
-          <Suspense fallback={<LoadingState />}>
-            <Route exact path="/twodimensionalvase" component={TwoDimensionalVase} />
-          </Suspense>
-          <Suspense fallback={<LoadingState />}>
-            <Route exact path="/metropolitan-arcadia" component={MetropolitanArcadia} />
-          </Suspense>
-        </div>
-      </>
-    </Router>
-  );
+	const style = {
+		height: "100vh",
+	};
+	const routesStyle = {
+		marginTop: `${
+			window.devicePixelRatio < 2 ? window.innerHeight / 125 : window.innerHeight / 255
+		}%`,
+	};
+	return (
+		<Router basename={process.env.PUBLIC_URL}>
+			<>
+				<Header></Header>
+				<div id="ROUTES_CONTAINER" style={style}>
+					<Route exact path="/" component={Main} />
+					<div style={routesStyle}>
+						<Route exact path="/about" component={Bio} />
+						<Suspense fallback={<LoadingState />}>
+							<Route exact path="/albania" component={Albania} />
+						</Suspense>
+						<Suspense fallback={<LoadingState />}>
+							<Route exact path="/bookholder" component={Bookholder} />
+						</Suspense>
+						<Suspense fallback={<LoadingState />}>
+							<Route exact path="/posy" component={Posy} />
+						</Suspense>
+						<Suspense fallback={<LoadingState />}>
+							<Route exact path="/spaceforone" component={SpaceForOne} />
+						</Suspense>
+						<Suspense fallback={<LoadingState />}>
+							<Route
+								exact
+								path="/twodimensionalvase"
+								component={TwoDimensionalVase}
+							/>
+						</Suspense>
+						<Suspense fallback={<LoadingState />}>
+							<Route
+								exact
+								path="/metropolitan-arcadia"
+								component={MetropolitanArcadia}
+							/>
+						</Suspense>
+					</div>
+				</div>
+			</>
+		</Router>
+	);
 };
 
 export default App;
